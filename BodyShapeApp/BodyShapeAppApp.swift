f981389154f8e35fcd0b6800694b0e51dea1ac89
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-20 17:32:38 +0300

    Initial Commit

---
+++ BodyShapeApp/BodyShapeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyShapeAppApp.swift
+//  BodyShapeApp
+//
+//  Created by mehmet Çelik on 20.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BodyShapeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
